bfcdbadb1519038ae8d4a552a5081d5b21387974
Carlos Mauro Chevez Alvarenga <cmauro2192@gmail.com>
2013-03-08 09:07:06 -0600

    Update config.php

--- application/config/config.php
+++ application/config/config.php
@@ -14,7 +14,7 @@
 | path to your installation.
 |
 */
-$config['base_url']	= 'http://sirese.site90.net//';
+$config['base_url']	= '';
 
 /*
 |--------------------------------------------------------------------------
