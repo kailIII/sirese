cc90160d96f21a8a25a667541b5465468fb04f76
Carlos Mauro Chevez Alvarenga <cmauro2192@gmail.com>
2013-03-08 09:05:32 -0600

    Update database.php

--- application/config/database.php
+++ application/config/database.php
@@ -48,10 +48,10 @@
 $active_group = 'default';
 $active_record = TRUE;
 
-$db['default']['hostname'] = 'mysql3.000webhost.com';
-$db['default']['username'] = 'a7898953_sirese';
-$db['default']['password'] = 'sirese_ugb';
-$db['default']['database'] = 'a7898953_sirese';
+$db['default']['hostname'] = '';
+$db['default']['username'] = '';
+$db['default']['password'] = '';
+$db['default']['database'] = '';
 $db['default']['dbdriver'] = 'mysql';
 $db['default']['dbprefix'] = '';
 $db['default']['pconnect'] = TRUE;
